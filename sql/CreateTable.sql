98889d0f11af00a81deab14ca48fb03c791d86ea
Vinay <vinshas1@gmail.com>
2020-10-22 16:41:18 +0530

    ok

--- sql/CreateTable.sql
+++ sql/CreateTable.sql
@@ -3,7 +3,7 @@ if not exists (select 1 from sys.tables where name='testtable')
 begin
 CREATE TABLE testtable(id int)
 end
-
+;
 if not exists (select 1 from sys.columns where name='testcol')
 begin
 alter table testtable
