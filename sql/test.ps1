e3cee8dd988780c8c602ead84624befd17f1348e
Vinay <vinshas1@gmail.com>
2020-10-22 16:35:33 +0530

    ok

--- sql/test.ps1
+++ sql/test.ps1
@@ -7,6 +7,8 @@ $rootfolder = $PSScriptRoot
 
 $rootfolder
 
+
+
 $files = get-childitem -Path $rootfolder -Filter "*.sql"
 
 foreach ($file in $files)
@@ -14,12 +16,4 @@ foreach ($file in $files)
  [string]$content = get-content -path $file.FullName
 
  Invoke-Sqlcmd -ServerInstance "b3a183f5d5b1"  -Database "master" -Query $content -Username $username -Password $pwd
-
 }
-    #$content = Get-Content -Path $file.
-
-
-
-
-
-#Invoke-Sqlcmd -ServerInstance "b3a183f5d5b1"  -Database "master" -Query "select 1" -Username $username -Password $pwd
