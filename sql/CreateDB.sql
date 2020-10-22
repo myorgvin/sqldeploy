9080e1b9390125e108f019f226cae3b96dda586e
Vinay <vinshas1@gmail.com>
2020-10-22 15:36:12 +0530

    k

--- sql/CreateDB.sql
+++ sql/CreateDB.sql
@@ -1,1 +1,5 @@
+
+if not exists (select 1 from sys.databases where name='testdb')
+begin
 CREATE DATABASE TESTDB
+end
--- sql/CreateTable.sql
+++ sql/CreateTable.sql
@@ -1,2 +1,5 @@
 USE TESTDB;
+if not exists (select 1 from sys.tables where name='testtable')
+begin
 CREATE TABLE testtable(id int)
+end
